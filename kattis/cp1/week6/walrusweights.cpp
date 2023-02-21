18eec4821b55f858f2605b1e1cc4eff2406d889a
Adrien Qi <aqi@Adriens-MacBook-Air.local>
2023-02-21 13:36:28 -0500

    week6

---
+++ kattis/cp1/week6/walrusweights.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n; cin >> n;
+    vector<int> inputs;
+    for (int i = 0; i < n; i++) {
+        cin
+    }
+
+    vector<int> dp(1001);
+    dp[0] = 
+    int count = 0;
+
+    while (count < 1000) {
+
+    }
+
+}
