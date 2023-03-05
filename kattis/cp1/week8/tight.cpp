7c0540491cb89aa08b33e92926b2af1950f5ab5c
adriencheese <adrien.qi304@gmail.com>
2023-03-05 17:09:23 -0500

    tight

--- kattis/cp1/week8/tight.cpp
+++ kattis/cp1/week8/tight.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -17,10 +18,31 @@ int main() {
     for (auto i : input) {
         vector<vector<int> > dp(i.second, vector<int>(i.first));
 
-        for (int j = 0; j < i.second; j++) {
-            for (int k = 0; k < i.first; k++) {
-                
+        for (int j =  0; j < i.first; j++) {
+            dp[j][0] = 1;
+        }
+
+        for (int j = 1; j < i.second; j++) { // index of word
+            for (int k = 0; k < i.first; k++) { // character
+                if (k == 0) {
+                    dp[k][j]  = dp[k][j - 1] + dp[k + 1][j - 1];
+                } else if (k == i.first) {
+                    dp[k][j]  = dp[k][j - 1] + dp[k - 1][j - 1];
+                } else {
+                    dp[k][j] = dp[k][j - 1] + dp[k - 1][j - 1] + dp[k + 1][j - 1];
+                }
             }
         }
+        
+        long double words = 0;
+        for (int j = 0; j < i.first; j++) {
+            words += dp[i.second - 1][j];
+        }
+
+        output.push_back(words / pow(i.first + 1, i.second));
+    }
+
+    for (auto i : output) {
+        cout << i << "\n";
     }
 }
