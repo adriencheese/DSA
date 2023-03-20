2d84a587d8bc02618c61a6da4556ff8a517616cc
Adrien Qi <adrien.qi304@gmail.com>
2023-03-20 11:03:18 -0400

    runningmom input done

---
+++ kattis/cp1/week9/runningmom.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    fstream FILE("input.txt");
+    string line;
+
+    int n; FILE >> n;
+    getline(FILE, line);
+
+    vector<pair<string, string> > list (n);
+
+    for (int i = 0; i < n; i++) {
+        // getline(FILE, line);
+        // stringstream s(line);
+        // s >> list[i].first >> list[i].second;
+        FILE >> list[i].first >> list[i].second;
+        cout << "check: " << list[i].first << list[i].second << endl;
+    }
+
+    vector<string> check;
+    FILE >> line;
+    check.push_back(line);
+
+    while (FILE >> line) {
+        check.push_back(line);
+    }
+
+    
+    
+}
