1b1f5fee7c45bca5da6d3b7b38ddbe0e7e4acf26
adriencheese <adrien.qi304@gmail.com>
2023-03-21 12:07:27 -0400

    unordered_map currentloop

--- kattis/cp1/week9/runningmom.cpp
+++ kattis/cp1/week9/runningmom.cpp
@@ -2,39 +2,95 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
-// if multiple edges from a node, add to a list of need to travel to
-// if just one edge, just traverse
+// if multiple edges from a node, as long as one path can loop, it is safe.
+// issue: city with multiple edges will get marked safe
 
 // hashmap for city names
+
+void dfs(string &in, unordered_map<string, vector<string> > &city_list, unordered_map<string, int> &safe_status, unordered_map<string, int> &current_loop) {
+    // cout << in << ": ";
+    // for (auto i : current_loop) {
+    //     cout << "("<< i.first << ", " << i.second << "), ";
+    // }
+    // cout << endl;
+    
+    if (current_loop[in] == 1) {
+        // cout << "safe" << endl;
+        for (auto i : current_loop) {
+            // cout << i << ", ";
+            if (i.second == 1) {
+                safe_status[i.first] = 1;
+            }
+        }
+        // cout << endl;
+
+        // current_loop.clear();
+        return;
+    }
+
+    current_loop[in] = 1;
+
+    for (auto i : city_list[in]) {
+        // cout << in << " to " << i << endl;
+        dfs(i, city_list, safe_status, current_loop);
+    }
+
+    if (!current_loop.empty()) {
+        current_loop[in] = -1;
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    fstream FILE("input.txt");
     string line;
 
-    int n; FILE >> n;
-    getline(FILE, line);
+    int n; cin >> n;
 
-    vector<pair<string, string> > list (n);
+    unordered_map<string, vector<string> > city_list;
+    unordered_map<string, int> safe_status;
 
     for (int i = 0; i < n; i++) {
-        FILE >> list[i].first >> list[i].second;
-        cout << "check: " << list[i].first << list[i].second << endl;
+        string first, second;
+        cin >> first >> second;
+
+        city_list[first].push_back(second);
+        safe_status[first] = -1;
+        safe_status[second] = -1;
     }
+    getline(cin, line);
 
     vector<string> check;
-    FILE >> line;
-    check.push_back(line);
 
-    while (FILE >> line) {
+    while (getline(cin, line)) {
+        if (line.empty()) break;
         check.push_back(line);
     }
 
+    for (auto i : check) {
+        unordered_map<string, int> current_loop;
 
-    
+        if (safe_status[i] == -1) {
+            // cout << "checcking\n";
+            dfs(i, city_list, safe_status, current_loop);
+        }
+
+        // cout << i << ": ";
+        // for (auto i : safe_status) {
+        //     cout << i.first << ", " << i.second << endl;
+        // }
+        // cout << endl;
+
+        if (safe_status[i] == 1) {
+            cout << i << " safe" << "\n";
+        } else {
+            cout << i << " trapped" << "\n";
+        }
+    }
 }
