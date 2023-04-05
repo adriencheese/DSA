5927c66e6d92451bd3c6273663633477a5321b46
Adrien Qi <adrien.qi304@gmail.com>
2023-04-04 20:47:25 -0400

    sync

--- kattis/cp1/week11/pickupsticks.cpp
+++ kattis/cp1/week11/pickupsticks.cpp
@@ -11,22 +11,17 @@ int main() {
     int n, m; cin >> n >> m;
 
     vector<vector<int> > list (n);
+    vector<int> degree(n, 0);
 
     for (int i = 0; i < m; i++) {
         int a, b; cin >> a >> b;
         list[a - 1].push_back(b - 1);
-    }
-
-    vector<int> degree(n, 0);
-    for (int i = 0; i < n; i++) {
-        for (auto i : list[i]) {
-            degree[i]++;
-        }
+        degree[b - 1]++;
     }
 
     vector<int> zeroes;
-    for (auto i : degree) {
-        if (i == 0) {
+    for (int i = 0; i < degree.size(); i++) {
+        if (degree[i] == 0) {
             zeroes.push_back(i);
         }
     }
@@ -38,14 +33,18 @@ int main() {
         sorted.push_back(temp);
         
         for (auto i : list[temp]) {
-            degree[i]--;
+            --degree[i];
             if (degree[i] == 0) {
                 zeroes.push_back(i);
             }
         }
     }
 
-    for (auto i : sorted) {
-        cout << i + 1 << "\n";
+    if (sorted.size() == n) {
+        for (auto i : sorted) {
+            cout << i + 1 << "\n";
+        }
+    } else {
+        cout << "IMPOSSIBLE" << "\n";
     }
 }
